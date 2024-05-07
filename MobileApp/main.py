67d90a18c939b327fdd963cc44c74b92e66dc93f
mc <maccandeias2@gmail.com>
2024-05-07 16:42:29 +0100

    Updates to mobile app

--- MobileApp/main.py
+++ MobileApp/main.py
@@ -1,15 +1,24 @@
 # Base Kivy Imports:
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.core.window import Window # Remove before py --> apk
-
 # Base KivyMD Imports:
 from kivymd.app import MDApp
-from kivymd.uix.list import TwoLineAvatarIconListItem
+
+# Dynamic UI:
+# Layout:
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.boxlayout import BoxLayout
+# Buttons & Labels:
+from kivymd.uix.list import TwoLineAvatarIconListItem, OneLineAvatarIconListItem, ThreeLineAvatarListItem
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.list import IconLeftWidget, IconRightWidget
+# Other Parts:
 from kivymd.uix.list import IRightBodyTouch
 from kivy.uix.popup import Popup
+from kivymd.uix.dialog import MDDialog
+
 
 import requests
 import logging
@@ -61,6 +70,35 @@ class MainScreen(Screen):
             debug_MobileApp.error(f'No Connection: {err}')
             print("No Connection!! ^_^")
 
+            items = []
+            items.append(
+                ThreeLineAvatarListItem(
+                    IconLeftWidget(
+                        icon='wifi-off'
+                    ),
+                    text="No Connection!",
+                    secondary_text='Check Internet Connection',
+                    tertiary_text='Or Try Again Later'
+                ),
+            )
+
+            # Popup:
+            self.dialog = MDDialog(
+                title="Failed to Connect:",
+                type="confirmation",
+                items=items,
+                content_cls=PopupError(),
+                buttons=[
+                    MDFlatButton(
+                        text="OK",
+                        theme_text_color="Custom",
+                        text_color=main_app.theme_cls.primary_color,
+                        on_release=lambda x: self.dialog.dismiss()
+                    ),
+                ],
+            )
+            self.dialog.open()
+
     def item_clicked(self, instance):
         id = instance.id
         debug_MobileApp.info(f'id[{id}]')
@@ -76,15 +114,53 @@ class MainScreen(Screen):
 
 
 class DeliverItem(Screen):
-    storage_picked = ObjectProperty(None)
+    # ids:
+    '''storage_picked = ObjectProperty(None)
     item_picked = ObjectProperty(None)
-    num_items  =ObjectProperty(None)
+    num_items  = ObjectProperty(None)'''
 
+    label_storage = StringProperty('')
+    label_item = StringProperty('')
+    num_items = 0
+    label_num_items = StringProperty('')
+    label_worker = StringProperty('')
+
+    def on_kv_post(self, *args):
+        self.reset_fields()
+
+    def reset_fields(self):
+        self.label_storage = 'Click to Pick'
+        self.label_item = 'Click to Pick'
+        self.num_items = 1
+        self.label_num_items = str(self.num_items)
+        self.label_worker = 'Click to Pick'
 
 
     def button_cancel(self):
+        # Reset all
+        self.reset_fields()
+        # Go back to main screen
         main_app.screen_manager.transition.direction = 'right'
         main_app.screen_manager.current = 'Main Screen'
+
+    def button_submit(self):
+        # Send data
+
+        # Go back to main screen
+        main_app.screen_manager.transition.direction = 'right'
+        main_app.screen_manager.current = 'Main Screen'
+
+    def button_plus(self):
+        print("plus")
+        self.num_items += 1
+        self.label_num_items = str(self.num_items)
+        print(f'{self.label_num_items = }')
+
+    def button_minus(self):
+        print("minus")
+        self.num_items -= 1
+        self.label_num_items = str(self.num_items)
+        print(f'{self.label_num_items = }')
 
 #TODO: Note: for the 'search' list[tuples] -> list[dict] like {'blue pen': 1} Use dict.keys -> list[keys] and use 'FuzzyWuzzy' ^_^
 
@@ -95,12 +171,15 @@ class YourContainer(IRightBodyTouch, MDBoxLayout):
     # use for TwoLineAvatarIconListItem: with 2 left items
     adaptive_width = True
 
+class PopupError(BoxLayout):
+    pass
+
 
 
 
 class Main(MDApp):
     def __init__(self, **kwargs):
-        self.title = "D&D Time!"
+        self.title = "Stock Control"
         super().__init__(**kwargs)
 
     def build(self):
